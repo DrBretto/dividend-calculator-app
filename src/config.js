10d89b2712fed35abf20c1443ddd4a2f0787eb5a
Brett Westerlund <drbretto82@gmail.com>
2020-09-08 11:44:00 -0400

    testing deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: "https://agile-peak-39361.herokuapp.com/api",
-  TOKEN_KEY: "super-secret-token",
+  TOKEN_KEY: "super-secret-secret",
+  API_KEY: process.env.REACT_APP_API_KEY,
 };
