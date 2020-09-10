d73c5845ebddbfcd32f91da0ff458087d50c9a59
Brett Westerlund <drbretto82@gmail.com>
2020-09-09 20:38:53 -0400

    responsive work

--- src/DetailsWindow/SummaryPanel.js
+++ src/DetailsWindow/SummaryPanel.js
@@ -6,11 +6,11 @@ export default class SummaryPanel extends React.Component {
   render() {
       return (
           <div className="light window">
-        <p>MSFT</p>
-        <p>10 Shares @ $213.02</p>
-        <p>Proposed Equity: $2130.20</p>
-        <p>Dividend Yield: 5.37%</p>
-        <p>EPS 1/5: 5.76 / 0.15</p>
+        <p className="red">MSFT</p>
+        <p className="green">10 Shares @ $213.02</p>
+        <p className="yellow">Proposed Equity: $2130.20</p>
+        <p className="light">Dividend Yield: 5.37%</p>
+        <p className="dark">EPS 1/5: 5.76 / 0.15</p>
       </div>
       );
   }
--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Footer extends React.Component {
   render() {
-    return <footer>Copyright ⓒ 2020 drbretto</footer>;
+    return <footer className="red window">Copyright ⓒ 2020 drbretto</footer>;
   }
 }
